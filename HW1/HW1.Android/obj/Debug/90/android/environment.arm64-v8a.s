7a48d2437f420a620872b5a3892bb879f1eb5d03
banysk <49277174+banysk@users.noreply.github.com>
2020-10-23 18:38:22 +1000

    finish cart

--- HW1/HW1.Android/obj/Debug/90/android/environment.arm64-v8a.s
+++ HW1/HW1.Android/obj/Debug/90/android/environment.arm64-v8a.s
@@ -73,7 +73,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"a3cbf9d0-78d8-4de2-af7f-f92eb0450bd6"
+	.asciz	"21d29d53-830e-432c-bd1c-9a3244e2463d"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
--- HW1/HW1/MainPage.xaml.cs
+++ HW1/HW1/MainPage.xaml.cs
@@ -15,16 +15,20 @@ namespace HW1
         public int Count { get; set; }
         public string PathToPic { get; set; }
     }
+
     public partial class MainPage : ContentPage
     {
         public static SortedDictionary<string, Good> goods = new SortedDictionary<string, Good>();
         public static bool bOpened = false;
-
+        public void UpdateListView()
+        {
+            var buf = goods.Select((a) => { return a.Value; }).ToList();
+            goods_list.ItemsSource = buf;
+        }
         public MainPage()
         {
             InitializeComponent();
-            var buf = goods.Select((a) => { return a.Value; }).ToList();
-            goods_list.ItemsSource = buf;
+            UpdateListView();
         }
         private void Btn_add_Clicked(object sender, EventArgs e)
         {   
@@ -34,14 +38,27 @@ namespace HW1
                 var cart = new AddPage();
                 Navigation.PushAsync(cart);
                 cart.Disappearing += (send, ev) => bOpened = false;
+                cart.Disappearing += (send, ev) => UpdateListView();
             }
         }
         async private void btn_order_Clicked(object sender, EventArgs e)
         {
             if (await DisplayAlert("Order", "Are you sure?", "Yes", "No"))
             {
+                goods.Clear();
+                UpdateListView();
                 await DisplayAlert("Order", "Ordered!", "Ok");
                 await Navigation.PopAsync();
+            }
+        }
+
+        async private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (await DisplayAlert("Remove", "Are you sure?", "Yes", "No"))
+            {
+                var btn = (Xamarin.Forms.Button)sender;
+                goods.Remove(btn.CommandParameter.ToString());
+                UpdateListView();
             }
         }
     }
